207b416bdb6f2c331d47dc1c91cfae9647b8f0a8
benjaminmendezinintech <benjaminmendezinintech@gmail.com>
2025-11-20 05:07:42 -0600

    base datos2

--- FINTECH/settings.py
+++ FINTECH/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'FINTECH.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'BenjaminmendezAr$DB_FINTECH',
+        'NAME':'DB_FINTECH',
         'USER':'BenjaminMendezAr',
         'PASSWORD':'Benjamin12a',
         'HOST':'BenjaminMendezArmenta.mysql.pythonanywhere-services.com',
